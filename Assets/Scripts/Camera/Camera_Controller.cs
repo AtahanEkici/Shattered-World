e78a4170f2b02919fcd18f4e764ef8e948f61266
Ata <atahanekici@hotmail.com>
2022-12-12 14:15:26 +0300

    Add files via upload

--- Assets/Scripts/Camera/Camera_Controller.cs
+++ Assets/Scripts/Camera/Camera_Controller.cs
@@ -1,7 +1,11 @@
-﻿using UnityEngine;
-
+﻿
+using UnityEngine;
 public class Camera_Controller : MonoBehaviour
 {
+    private float camera_x_rotation;
+    private float camera_y_rotation;
+    private float camera_z_rotation;
+
     [Header("Mouse Middle Button Inputs")]
     [SerializeField] private float Magnify_speed = 100f;
     [SerializeField] private float max_magnify = 0.2f;
@@ -20,6 +24,13 @@ public class Camera_Controller : MonoBehaviour
     [SerializeField] private float max_horizontal = 100f;
     [SerializeField] private float min_horizontal = 100f;
     */
+    private void Awake()
+    {
+        camera_x_rotation = transform.localEulerAngles.x;
+        camera_y_rotation = transform.localEulerAngles.y;
+        camera_z_rotation = transform.localEulerAngles.z;
+    }
+
     private void LateUpdate()
     {
         MoveWithKeyboard();
@@ -28,46 +39,53 @@ public class Camera_Controller : MonoBehaviour
     }
     private void MoveWithMouse()
     {
-        float x = Input.GetAxis("Mouse X");
-        float y = Input.GetAxis("Mouse Y");
+        float x = Input.GetAxisRaw("Mouse X");
+        float y = Input.GetAxisRaw("Mouse Y");
 
         if(Input.GetMouseButton(0)) // traverse the plane //
         {
+            /*
             if (x == 0) { return; }
 
             if(InvertControlls)
             {
-                transform.Translate(x * MouseMoveSpeed * Time.unscaledDeltaTime * Vector3.right);
+                transform.Translate(x * MouseMoveSpeed * Time.deltaTime * Vector3.right);
             }
             else
             {
-                transform.Translate(-x * MouseMoveSpeed * Time.unscaledDeltaTime * Vector3.right);
+                transform.Translate(-x * MouseMoveSpeed * Time.deltaTime * Vector3.right);
             }
+            */
         }
         else if (Input.GetMouseButton(1)) // rotate camera with left click //
         {
+            /*
             Vector3 rotation = transform.rotation.eulerAngles;
             transform.rotation = Quaternion.Euler((y+rotation.x), (x+rotation.y), 0);
+            */
         }
     }
     private void MoveWithKeyboard() // the script that moves the camera through keyboard input //
     {
         float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
-
-        Vector3 moveBy = transform.right * x + transform.forward * y;
-        transform.Translate(KeyboardMoveSpeed * Time.unscaledDeltaTime * moveBy.normalized);
+        //if (x == 0 && y == 0) { return; }
+        Vector3 moveBy = (Vector3.right * x) + ((Vector3.up+Vector3.forward) * 0.5f * y);
+        //Vector3 bisaction = (moveBy.normalized / 2.0f);
+        //bisaction = (Quaternion.Euler(camera_x_rotation, camera_y_rotation, camera_z_rotation) * bisaction).normalized;
+        Debug.DrawRay(Camera.main.transform.position, Vector3.forward * 100f,Color.black);
+        transform.Translate(KeyboardMoveSpeed * Time.deltaTime * moveBy.normalized);
     }
 
     private void Magnify()
     {
-        float MouseWheelInput = Input.GetAxis("Mouse ScrollWheel");
+        float MouseWheelInput = Input.GetAxisRaw("Mouse ScrollWheel");
 
         // Constraints - START// 
         if (MouseWheelInput == 0f) { return; } // if mouse 3 not engaged  do not execute//
         float z = transform.position.z;
-        if (z < min_magnify && MouseWheelInput < 0f) { Debug.Log("minimum magnification reached"); return; }
-        if (z > max_magnify && MouseWheelInput > 0f) { Debug.Log("maximum magnification reached"); return; }
+        if (z < min_magnify && MouseWheelInput < 0f) { z = min_magnify; Debug.Log("minimum magnification reached"); return; }
+        if (z > max_magnify && MouseWheelInput > 0f) { z = max_magnify; Debug.Log("maximum magnification reached"); return; }
         // Constraints - END // 
 
         transform.Translate(Magnify_speed * MouseWheelInput * Time.unscaledDeltaTime * Vector3.forward);
