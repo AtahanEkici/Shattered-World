758796f96942a681e244186550222e32d555c29b
Ata <atahanekici@hotmail.com>
2022-12-12 16:50:22 +0300

    Add files via upload

--- Assets/Scripts/Camera/Camera_Controller.cs
+++ Assets/Scripts/Camera/Camera_Controller.cs
@@ -1,42 +1,79 @@
-﻿
-using UnityEngine;
+﻿using UnityEngine;
 public class Camera_Controller : MonoBehaviour
 {
-    private float camera_x_rotation;
-    private float camera_y_rotation;
-    private float camera_z_rotation;
-
-    [Header("Mouse Middle Button Inputs")]
-    [SerializeField] private float Magnify_speed = 100f;
-    [SerializeField] private float max_magnify = 0.2f;
-    [SerializeField] private float min_magnify = -10f;
-
-    [Header("Mouse Left Click Inputs")]
-    [SerializeField] private bool InvertControlls = false;
-    [SerializeField] private float MouseMoveSpeed = 10f;
+    [Header("Mouse Inputs")]
+    [SerializeField]private float Magnify_speed = 100f;
+    [SerializeField]private float max_magnify = 0.2f;
+    [SerializeField]private float min_magnify = -10f;
+    //[SerializeField] private bool InvertControlls = false;
+    [SerializeField]private float MouseMoveSpeed = 100f;
+    [SerializeField]private float MouseInputTemp = 0f;
+    
 
     [Header("Keyboard  Inputs")]
     [SerializeField] private float KeyboardMoveSpeed = 100f;
-    /*
-    [SerializeField] private float max_vertical = 100f;
-    [SerializeField] private float mim_vertical = 100f;
-
-    [SerializeField] private float max_horizontal = 100f;
-    [SerializeField] private float min_horizontal = 100f;
-    */
-    private void Awake()
-    {
-        camera_x_rotation = transform.localEulerAngles.x;
-        camera_y_rotation = transform.localEulerAngles.y;
-        camera_z_rotation = transform.localEulerAngles.z;
-    }
 
     private void LateUpdate()
     {
-        MoveWithKeyboard();
+       //MoveWithKeyboard();
         MoveWithMouse();
-        Magnify();
+       // Magnify();
     }
+
+    private void MoveWithMouse()
+    {
+        /*
+        float x = Input.GetAxisRaw("Mouse X");
+        float y = Input.GetAxisRaw("Mouse Y");
+
+        if (x == 0 && y == 0) { return; }
+        */
+
+        Vector3 mousepos = Input.mousePosition;
+
+        float mouse_x = mousepos.x;
+        float mouse_y = mousepos.y;
+
+        float width = Screen.width;
+        float height = Screen.height;
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Width: "+width+" - Mouse_x: "+mouse_x+"");
+        }
+
+        if(mouse_x <= (0 + MouseInputTemp))
+        {
+            Debug.Log("Sol");
+            transform.Translate(-1f * MouseMoveSpeed * Time.unscaledDeltaTime * Vector3.right);
+        }
+        else if(mouse_x >= (width - MouseInputTemp))
+        {
+            Debug.Log("Sağ");
+            transform.Translate(MouseMoveSpeed * Time.unscaledDeltaTime * Vector3.right); 
+        }
+
+        if (mouse_y >= (height - MouseInputTemp))
+        {
+            Debug.Log("Yukarı");
+            transform.Translate(MouseMoveSpeed * Time.unscaledDeltaTime * (0.5f * (Vector3.up + Vector3.forward)));
+        }
+        else if (mouse_y <= (0 + MouseInputTemp))
+        {
+            Debug.Log("Aşağı");
+            transform.Translate(-1f * MouseMoveSpeed * Time.unscaledDeltaTime * (0.5f * (Vector3.up + Vector3.forward)));
+        }
+
+        /*
+        if(mouse_x >= (width - MouseInputTemp) || mouse_y >= (height - MouseInputTemp) || mouse_x  <= (0 + MouseInputTemp) || mouse_y <= (0 + MouseInputTemp))
+        {
+            Debug.Log("X:"+x+", Y: "+y+"");
+            Vector3 moveBy = (Vector3.right * x) + (0.5f * y * (Vector3.up + Vector3.forward));
+            transform.Translate(MouseMoveSpeed * Time.deltaTime * moveBy.normalized);
+        }
+        */
+;    }
+    /*
     private void MoveWithMouse()
     {
         float x = Input.GetAxisRaw("Mouse X");
@@ -44,7 +81,6 @@ public class Camera_Controller : MonoBehaviour
 
         if(Input.GetMouseButton(0)) // traverse the plane //
         {
-            /*
             if (x == 0) { return; }
 
             if(InvertControlls)
@@ -54,25 +90,21 @@ public class Camera_Controller : MonoBehaviour
             else
             {
                 transform.Translate(-x * MouseMoveSpeed * Time.deltaTime * Vector3.right);
-            }
-            */
+            } 
         }
         else if (Input.GetMouseButton(1)) // rotate camera with left click //
         {
-            /*
             Vector3 rotation = transform.rotation.eulerAngles;
-            transform.rotation = Quaternion.Euler((y+rotation.x), (x+rotation.y), 0);
-            */
+            transform.rotation = Quaternion.Euler((y+rotation.x), (x+rotation.y), 0); 
         }
     }
+    */
     private void MoveWithKeyboard() // the script that moves the camera through keyboard input //
     {
         float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
-        //if (x == 0 && y == 0) { return; }
+        if (x == 0 && y == 0) { return; }
         Vector3 moveBy = (Vector3.right * x) + ((Vector3.up+Vector3.forward) * 0.5f * y);
-        //Vector3 bisaction = (moveBy.normalized / 2.0f);
-        //bisaction = (Quaternion.Euler(camera_x_rotation, camera_y_rotation, camera_z_rotation) * bisaction).normalized;
         Debug.DrawRay(Camera.main.transform.position, Vector3.forward * 100f,Color.black);
         transform.Translate(KeyboardMoveSpeed * Time.deltaTime * moveBy.normalized);
     }
